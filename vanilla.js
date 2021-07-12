436081c9f2eac1de6f9e4aecddbe00495f8ace23
Kent Zamudio <kazamudio@up.edu.ph>
2021-07-12 17:26:33 +0800

    complete site deployed at firebase

--- vanilla.js
+++ vanilla.js
@@ -1,20 +1,22 @@
 let menu = document.querySelector('svg.menu-collapse');
 let menuClose = document.querySelector('svg.menu-shown');
 let navBar = document.querySelector('nav');
+let navOpen = false;
 
-menu.addEventListener('click', event => {
+menu.addEventListener('click', (event) => {
+	navOpen = true;
 	menuClose.classList.remove('hidden');
 	menu.classList.add('hidden');
 	navBar.style.transform = 'translateX(0)';
-	console.log(event);
 });
 menuClose.addEventListener('click', () => {
+	navOpen = false;
 	menu.classList.remove('hidden');
 	menuClose.classList.add('hidden');
 	navBar.style.transform = 'translateX(100%)';
 });
 
-window.addEventListener('resize', event => {
+window.addEventListener('resize', (event) => {
 	if (event.target.innerWidth >= 720) {
 		navBar.style.transform = '';
 	} else {
